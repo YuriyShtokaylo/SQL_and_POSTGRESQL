154651da4aff640531f07b038351766c7c8fdea0
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-02-12 20:05:56 +0100

    Introduce DDL and DML

--- select.sql
+++ select.sql
@@ -10,3 +10,18 @@ LETS SUMMORIZE:
 - SQL statements are case-insensitive - SELECT = select = SeLeCt
 - Identifiers (e.g. table names, column names) can be surrounded with quotes to avoid clashes with build-in keywords
 - Statement can consist of one or more clauses - but the order of clauses must be correct(e.g. SELECT before FROM and WHERE)
+*/
+
+--We have 2 groops of rules:
+/*
+DDL(DATA DEFINITION LANGUAGE):
+- Query statements that define DB, tables & table structure
+- Table constraints & relationships
+- DB & table management(e.g. altering tables, droping tables)
+*/
+/*
+DML(DATA MANIPULATION LANGUAGE) - Lets refer it as CRUD(Create Read Update Delete)
+- Query statements that manipulate (insert, update & delete) or fetch data
+- May join, filter or sort data
+- Use DB & tables that where created via "Data Definition Statements"
+*/
