1685653db2781fea6ff3cfba215b4f737e681d30
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-02-12 19:59:29 +0100

    Describe Core SQL rules

--- select.sql
+++ select.sql
@@ -1,1 +1,12 @@
-SELECT email FROM customers;
+SELECT 'email' FROM 'customers';
+SELECT 'first_name' FROM 'customers';
+SELECT "select" FROM "customers";
+--THIS IS COMMENT(INLINE)
+/*THIS IS BLOCK COMMENT
+
+LETS SUMMORIZE:
+- SQL statements must end with senicolons - if you have more than one SQL statement in command
+- A single SQL statement in a command doesn't need a semicolon at the end
+- SQL statements are case-insensitive - SELECT = select = SeLeCt
+- Identifiers (e.g. table names, column names) can be surrounded with quotes to avoid clashes with build-in keywords
+- Statement can consist of one or more clauses - but the order of clauses must be correct(e.g. SELECT before FROM and WHERE)
