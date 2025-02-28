dcdd3b07b15252baf18b1fc6f2eba2bfab20a480
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-02-28 23:39:31 +0100

    For Postgress

--- DDL/02-create-users-table.sql
+++ DDL/02-create-users-table.sql
@@ -1,5 +1,9 @@
+-- Postgress + ENUM => We have to create a custom type first
+CREATE TYPE employment_status AS ENUM('employed', 'self-employed', 'unemployed');
+
 CREATE TABLE IF NOT EXISTS users (
     full_name VARCHAR(200),
     yearly_salary INT,
-    `status` ENUM('employed', 'self-employed', 'unemployed') -- MySQL syntax
+    --`status` ENUM('employed', 'self-employed', 'unemployed') -- MySQL syntax
+    current_status employment_status
 );
