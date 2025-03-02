bcea131587c1ec61c0dbd89fde6b3fd0cb0811d0
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-03-02 23:04:03 +0100

    Use CHECK 1

--- DDL/02-create-users-table.sql
+++ DDL/02-create-users-table.sql
@@ -3,7 +3,7 @@ CREATE TYPE employment_status AS ENUM('employed', 'self-employed', 'unemployed')
 
 CREATE TABLE IF NOT EXISTS users (
     full_name VARCHAR(200) NOT NULL,
-    yearly_salary INT DEFAULT NULL,
+    yearly_salary INT CHECK (yearly_salary > 0) DEFAULT NULL,
     -- current_status ENUM('employed', 'self-employed', 'unemployed') -- MySQL syntax
     current_status employment_status
 );
