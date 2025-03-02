5866c3af4d79efe887f48b607c49de26491106b2
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-03-02 22:28:21 +0100

    Fix typos

--- DDL/13-constraints.sql
+++ DDL/13-constraints.sql
@@ -1,7 +1,7 @@
 ALTER TABLE users
-ALTER COLUMN full_name SET NOT NULL,          --
+/* ALTER COLUMN full_name SET NOT NULL,          --
 ALTER COLUMN current_status SET NOT NULL,     -- Postgress
-ALTER COLUMN yearly_salary SET DEFAULT NULL;  --
-/* MODIFY COLUMN full_name  VARCHAR(300) SET NOT NULL,                    --
-MODIFY COLUMN ENAM('employed', 'self-employed', 'unemployed') NOT NULL,   --  MySQL
-MODIFY COLUMN yearly_salary INT DEFAULT NULL;*/                           --
+ALTER COLUMN yearly_salary SET DEFAULT NULL;  -- */
+MODIFY COLUMN full_name  VARCHAR(300) NOT NULL,                       --
+MODIFY COLUMN current_status ENUM('employed', 'self-employed', 'unemployed') NOT NULL,   --  MySQL
+MODIFY COLUMN yearly_salary INT DEFAULT NULL;                             --
