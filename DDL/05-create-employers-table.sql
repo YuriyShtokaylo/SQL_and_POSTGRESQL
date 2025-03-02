984cebcc7eaeb60496de97fe03bfa6e3cba69cc5
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-03-02 16:51:46 +0100

    Modify creation queries

--- DDL/05-create-employers-table.sql
+++ DDL/05-create-employers-table.sql
@@ -3,5 +3,5 @@ CREATE TABLE employers (
     company_address VARCHAR(300),
     -- yearly_revenu FLOAT(5, 2), -- Approximation[ Allowed: 123.12, 12.1 | Not allowed: 1234.2 | 1.123 =Rounde=> 1.12 ]
     yearly_revenu NUMERIC(5,2), -- Exact value[ Allowed: 123.12 | Not allowed: 1234.123 ]
-    is_hiring BOOLEAN
+    is_hiring BOOLEAN DEFAULT FALSE
 )
